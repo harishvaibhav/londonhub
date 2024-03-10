2e69f667f6380bedca38cd8211c4c5ca4685c3b2
harish vaibhav nethi <nethiharishvaibhav@gmail.com>
2024-03-10 17:04:04 +0000

    migration files and routes

---
+++ database/migrations/2024_03_10_170038_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->text('title'); // title for the post
+            $table->text('body'); // content for the post
+            $table->text('user_id'); // for releationship with the user
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AnalyticController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +12,38 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('', [AuthController::class, 'index'])->name('login');
+
+
+// Authentication Routes
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
+Route::get('dashboard', [AuthController::class, 'dashboard']);
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+// End of Authentication Routes
+
+// resource which is laravel helper whichi will helps us to generate REST API structured routes
+// Post Related Routes 
+Route::resource("posts", PostController::class);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}/edit', [PostController::class, 'update']);
+
+// Comments Related Routes
+Route::resource("comments", CommentController::class);
+
+
+// Analytics Related Routes
+Route::resource("analytics", AnalyticController::class);
+Route::get("/view-analytics", [AnalyticController::class, 'showAnalytics']);
