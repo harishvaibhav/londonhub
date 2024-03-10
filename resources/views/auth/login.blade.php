6cddc53a072271797d0338764e7257f0c22dd1b3
harish vaibhav nethi <nethiharishvaibhav@gmail.com>
2024-03-10 18:13:32 +0000

    intermediate

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -82,7 +82,7 @@
                             class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out  bg-gradient-to-r from-cyan-500 to-blue-500"
                             data-mdb-ripple="true" data-mdb-ripple-color="light">Sign up for free</button></a>
                 </div>
-                
+
             </form>
         </div>
     </div>
--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1,77 +1,114 @@
 @extends('layouts.app')
 
 @section('content')
-<main class="login-form">
-    <div class="cotainer">
-        <div class="row justify-content-center mt-10">
-            <div class="col-md-8">
-                <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <div class="card-header text-2xl text-center mtb-5 ">Create an Account</div>
-                    <div class="card-body">
 
-                        <form action="{{ route('register.post') }}" method="POST">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="name" class="font-bold text-gray-800 mt-3 col-md-4 col-form-label text-md-right">Name</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="name" class="form-control rounded-lg shadow-lg" name="name" required autofocus>
-                                    @if ($errors->has('name'))
-                                    <span class="text-danger">{{ $errors->first('name') }}</span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="email_address" class="font-bold text-gray-800 mt-3 col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="email_address" class="form-control rounded-lg shadow-lg" name="email" required autofocus>
-                                    @if ($errors->has('email'))
-                                    <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="font-bold text-gray-800 mt-3 col-md-4 col-form-label text-md-right">Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" id="password" class="form-control rounded-lg shadow-lg" name="password" required>
-                                    @if ($errors->has('password'))
-                                    <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="password" class="font-bold text-gray-800 mt-3 col-md-4 col-form-label text-md-right">Role</label>
-                                <div class="col-md-6">
-                                    <select id="email" class="block mt-1 w-full" name="role" required>
-                                        <option value="user">User</option>
-                                        <option value="admin">Admin</option>
-                                    </select>
-
-                                </div>
-                            </div>
-
-
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-                                    Create Account
-                                </button>
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
+<div class="flex flex-row w-full items-center h-screen">
+    <div class="w-1/2 bg-pink-100">
+        <div class="w-full flex flex-col items-center h-screen justify-center">
+            <div class="items-center">
+                <svg viewBox="81.51 79.5872 176.98 90.8255" width="176.98" height="90.826"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <g data-v-0dd9719b="" fill="#333" class="icon-text-wrapper icon-svg-group iconsvg"
+                        transform="matrix(1, 0, 0, 1, 81.51000213623047, 79.5872344970703)">
+                        <g class="iconsvg-imagesvg" transform="translate(58.4899959564209,0)">
+                            <g>
+                                <rect fill="#333" fill-opacity="0" stroke-width="2" x="0" y="0" width="60"
+                                    height="62.365524338145875" class="image-rect" />
+                                <svg x="0" y="0" width="60" height="62.365524338145875" filtersec="colorsb9094439198"
+                                    class="image-svg-svg primary" style="overflow: visible;">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0.0026211319491267204 0 104.36734008789062 108.48165130615234">
+                                        <title>资源 139</title>
+                                        <path
+                                            d="M58.5 4.44A13.51 13.51 0 0 1 62 8.09c7.17 11.38-16.15 53.76-19.83 61.44-1.56 3.29 1.4 5.87 11.29 4.74 27.29-1 36.1 7.56 38.15 16.21a52.19 52.19 0 0 0-33.13-86z"
+                                            fill="#eda837" />
+                                        <path
+                                            d="M20.3 89.73c-16.92-.45 27.62-48.55 30.57-69.86C53.38 1.75 33.07 0 33.07 0a12.44 12.44 0 0 1 7.66 5.34 52.21 52.21 0 1 0 40.79 94.14c3.8 1.52 7.1 4 7.7 8 1.35-24.64-53.9-17.35-68.92-17.75z"
+                                            fill="#24669e" />
+                                    </svg>
+                                </svg>
+                            </g>
+                        </g>
+                        <g transform="translate(0,69.36552429199219)">
+                            <g data-gra="path-name" fill-rule="" class="tp-name iconsvg-namesvg">
+                                <g transform="scale(1)">
+                                    <g>
+                                        <path
+                                            d="M7.25-21.16L7.25-4.23 18.29-4.23 18.29 0 1.81 0 1.81-21.16 7.25-21.16ZM36.42-11.49L36.42-4.53Q36.42-2.27 35.14-0.98 33.85 0.3 31.59 0.3L31.59 0.3 24.63 0.3Q22.37 0.3 21.08-0.98 19.8-2.27 19.8-4.53L19.8-4.53 19.8-11.49Q19.8-13.75 21.08-15.04 22.37-16.32 24.63-16.32L24.63-16.32 31.59-16.32Q33.85-16.32 35.14-15.04 36.42-13.75 36.42-11.49L36.42-11.49ZM31.13-4.99L31.13-11.03Q31.13-12.24 29.92-12.24L29.92-12.24 26.3-12.24Q25.09-12.24 25.09-11.03L25.09-11.03 25.09-4.99Q25.09-3.78 26.3-3.78L26.3-3.78 29.92-3.78Q31.13-3.78 31.13-4.99L31.13-4.99ZM49.12-11.94L48.06-11.94Q46.46-11.94 44.43-11.34L44.43-11.34 44.43 0 39.14 0 39.14-16.02 43.83-16.02 44.13-14.51Q45.55-15.45 47.05-15.88 48.54-16.32 49.57-16.32L49.57-16.32 50.93-16.32Q52.99-16.32 54.23-15.08 55.47-13.84 55.47-11.79L55.47-11.79 55.47 0 50.18 0 50.18-10.88Q50.18-11.34 49.87-11.64 49.57-11.94 49.12-11.94L49.12-11.94ZM74.21 0L69.52 0 69.22-1.51Q67.8-0.57 66.3-0.14 64.81 0.3 63.78 0.3L63.78 0.3 62.42 0.3Q60.36 0.3 59.12-0.94 57.88-2.18 57.88-4.23L57.88-4.23 57.88-11.18Q57.88-13.45 59.17-14.74 60.45-16.02 62.72-16.02L62.72-16.02 68.92-16.02 68.92-21.16 74.21-21.16 74.21 0ZM64.23-4.08L65.29-4.08Q66.89-4.08 68.92-4.69L68.92-4.69 68.92-11.94 64.38-11.94Q63.17-11.94 63.17-10.73L63.17-10.73 63.17-5.14Q63.17-4.69 63.48-4.38 63.78-4.08 64.23-4.08L64.23-4.08ZM93.55-11.49L93.55-4.53Q93.55-2.27 92.27-0.98 90.98 0.3 88.72 0.3L88.72 0.3 81.76 0.3Q79.5 0.3 78.21-0.98 76.93-2.27 76.93-4.53L76.93-4.53 76.93-11.49Q76.93-13.75 78.21-15.04 79.5-16.32 81.76-16.32L81.76-16.32 88.72-16.32Q90.98-16.32 92.27-15.04 93.55-13.75 93.55-11.49L93.55-11.49ZM88.26-4.99L88.26-11.03Q88.26-12.24 87.05-12.24L87.05-12.24 83.43-12.24Q82.22-12.24 82.22-11.03L82.22-11.03 82.22-4.99Q82.22-3.78 83.43-3.78L83.43-3.78 87.05-3.78Q88.26-3.78 88.26-4.99L88.26-4.99ZM106.25-11.94L105.19-11.94Q103.59-11.94 101.56-11.34L101.56-11.34 101.56 0 96.27 0 96.27-16.02 100.96-16.02 101.26-14.51Q102.68-15.45 104.18-15.88 105.67-16.32 106.7-16.32L106.7-16.32 108.06-16.32Q110.12-16.32 111.36-15.08 112.59-13.84 112.59-11.79L112.59-11.79 112.59 0 107.3 0 107.3-10.88Q107.3-11.34 107-11.64 106.7-11.94 106.25-11.94L106.25-11.94ZM129.67-21.16L129.67-15.72Q132.6-16.32 134.81-16.32L134.81-16.32 136.17-16.32Q138.23-16.32 139.47-15.08 140.71-13.84 140.71-11.79L140.71-11.79 140.71 0 135.42 0 135.42-11.18Q135.42-11.64 135.11-11.94 134.81-12.24 134.36-12.24L134.36-12.24 133.3-12.24Q132.15-12.24 131.2-12.12 130.25-12 129.67-11.94L129.67-11.94 129.67 0 124.38 0 124.38-21.16 129.67-21.16ZM149.77-4.08L150.83-4.08Q152.43-4.08 154.46-4.69L154.46-4.69 154.46-16.02 159.75-16.02 159.75 0 155.06 0 154.76-1.51Q153.34-0.57 151.84-0.14 150.35 0.3 149.32 0.3L149.32 0.3 147.96 0.3Q145.9 0.3 144.66-0.94 143.43-2.18 143.43-4.23L143.43-4.23 143.43-16.02 148.72-16.02 148.72-5.14Q148.72-4.69 149.02-4.38 149.32-4.08 149.77-4.08L149.77-4.08ZM173.5-5.29L173.5-11.18Q173.5-11.64 173.2-11.94 172.9-12.24 172.44-12.24L172.44-12.24 171.69-12.24Q170.42-12.24 169.5-12.12 168.57-12 168.06-11.94L168.06-11.94 168.06-4.08 172.29-4.08Q173.5-4.08 173.5-5.29L173.5-5.29ZM168.06-21.16L168.06-15.72Q170.99-16.32 173.2-16.32L173.2-16.32 174.26-16.32Q176.31-16.32 177.55-15.08 178.79-13.84 178.79-11.79L178.79-11.79 178.79-4.84Q178.79-2.57 177.51-1.28 176.22 0 173.95 0L173.95 0 162.77 0 162.77-21.16 168.06-21.16Z"
+                                            transform="translate(-1.809999942779541, 21.15999984741211)" />
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
             </div>
         </div>
     </div>
-</main>
+    <div class="w-1/2 p-10 h-screen">
+        <div class="flex flex-col justify-center h-screen p-5">
+            <div
+                class="card-header text-2xl text-center mb-5 bg-gradient-to-r text-white from-cyan-500 to-blue-500 p-2 rounded rounded-xl">
+                Create an Account</div>
+            <div class="card-body">
+                <form action="{{ route('register.post') }}" method="POST">
+                    @csrf
+                    <div class="form-group row">
+                        <label for="name"
+                            class="font-bold text-gray-800 mt-3 col-md-4 col-form-label text-md-right">Name</label>
+                        <div class="col-md-6">
+                            <input type="text" id="name" class="form-control rounded-lg shadow-lg" name="name" required
+                                autofocus>
+                            @if ($errors->has('name'))
+                            <span class="text-danger">{{ $errors->first('name') }}</span>
+                            @endif
+                        </div>
+                    </div>
 
-<footer class="text-center lg:text-left bg-gray-100 text-gray-600 fixed bottom-0">
-    <div class="text-center p-6 bg-gray-200">
-        <span>© 2023 Blogger</span>
-        <a class="text-gray-600 font-semibold" href="https://Blogger-elements.com/">Blogger Elements</a>
+                    <div class="form-group row">
+                        <label for="email_address"
+                            class="font-bold text-gray-800 mt-3 col-md-4 col-form-label text-md-right">E-Mail
+                            Address</label>
+                        <div class="col-md-6">
+                            <input type="text" id="email_address" class="form-control rounded-lg shadow-lg" name="email"
+                                required autofocus>
+                            @if ($errors->has('email'))
+                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password"
+                            class="font-bold text-gray-800 mt-3 col-md-4 col-form-label text-md-right">Password</label>
+                        <div class="col-md-6">
+                            <input type="password" id="password" class="form-control rounded-lg shadow-lg"
+                                name="password" required>
+                            @if ($errors->has('password'))
+                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                   <div class="text-center mt-10">
+                        <button type="submit" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out
+                            bg-gradient-to-r from-cyan-500 to-blue-500">
+                            Create Account
+                        </button>
+                       
+                    </div>
+                    <div class="text-center mt-10">
+                        <a href="/login" type="submit" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out
+                                                                            bg-gradient-to-r from-purple-500 to-pink-500">
+                            Back to login
+                        </a>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     </div>
-</footer>
+</div>
+
 
 
 @endsection
