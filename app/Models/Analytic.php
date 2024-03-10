d83fd6097838ca9139c45ad0290d171f61bafe07
harish vaibhav nethi <nethiharishvaibhav@gmail.com>
2024-03-10 16:10:23 +0000

    User, Comment, Post, Analytic model and layut

---
+++ app/Models/Analytic.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Analytic extends Model
+{
+    // allowing these parameters to be updated
+    protected $fillable = ['post_id', 'view_count'];
+    use HasFactory;
+}
